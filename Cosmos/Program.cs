e83eb512a80fe9286f54a42a41a7e02f2619cb0e
vanisacia <i8591d@gmail.com>
2019-10-14 20:56:47 +0100

    Initial

---
+++ Cosmos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CosmosLib;
+namespace Cosmos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<CosmoProgram> prog = new List<CosmoProgram>();
+            CosmoProgram cp = new CosmoProgram();
+            cp.CosmoID = 1;
+            cp.Name = "GDSPAF";
+
+            List<CosmosProgramData> p = new List<CosmosProgramData>
+            {
+                new CosmosProgramData
+                {
+                    CategoryName = "Memory Infor",
+                    CategoryValue="23er"
+                },
+                new CosmosProgramData
+                {
+                    CategoryName = "DSP",
+                    CategoryValue="3453"
+                }
+               
+            };
+
+            cp.Data = p;
+            prog.Add(cp);
+            Cosmo.CreateProgram(prog);
+        }
+    }
+}
